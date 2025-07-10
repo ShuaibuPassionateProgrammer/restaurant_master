21f31f40f7c9071c2ac97e275720032fb8fad585
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-10 11:23:34 +0100

    Working on header file

--- index.php
+++ index.php
@@ -1,16 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <!-- Important to make website responsive -->
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Restaurant Website</title>
-
-    <!-- Link our CSS file -->
-    <link rel="stylesheet" href="css/style.css">
-</head>
-
-<body>
+<?php include("./includes/header.php"); ?>
     <!-- Navbar Section Starts Here -->
     <section class="navbar">
         <div class="container">
