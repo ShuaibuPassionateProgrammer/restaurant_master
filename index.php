5e5c7fd334566f0bc6bec180de10abdab4baf0c5
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-10 12:24:37 +0100

    Working on footer file

--- index.php
+++ index.php
@@ -214,14 +214,4 @@
         </div>
     </section>
     <!-- social Section Ends Here -->
-
-    <!-- footer Section Starts Here -->
-    <section class="footer">
-        <div class="container text-center">
-            <p>All rights reserved. Designed By <a href="#">Vijay Thapa</a></p>
-        </div>
-    </section>
-    <!-- footer Section Ends Here -->
-
-</body>
-</html>
+<?php include("includes/footer.php"); ?>
