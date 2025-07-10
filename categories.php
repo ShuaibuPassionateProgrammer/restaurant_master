92a69715ea8444e1b3e7ca2c8ffb8c14019817b7
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-07-10 12:21:37 +0100

    Working on footer file

--- categories.php
+++ categories.php
@@ -158,13 +158,3 @@
     </section>
     <!-- social Section Ends Here -->
 
-    <!-- footer Section Starts Here -->
-    <section class="footer">
-        <div class="container text-center">
-            <p>All rights reserved. Designed By <a href="#">Vijay Thapa</a></p>
-        </div>
-    </section>
-    <!-- footer Section Ends Here -->
-
-</body>
-</html>
